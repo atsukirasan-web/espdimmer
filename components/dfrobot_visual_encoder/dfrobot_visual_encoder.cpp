79faf0f91f94886c7e6afbc2c2f60c46a0de6f44
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 15:25:13 +0000

    Update dfrobot_visual_encoder.cpp

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.cpp
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.cpp
@@ -1,8 +1,22 @@
-#include "dfrobot_visual_encoder.h"
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
-namespace dfrobot_visual_encoder {}
+namespace dfrobot_visual_encoder {
 
-// Nothing extra needed — header contains full logic
+class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void loop() override;
 
+  void set_led_level(float level);
+
+ protected:
+  int16_t last_value_{0};
+  uint32_t last_button_{0};
+};
+
+}  // namespace dfrobot_visual_encoder
 }  // namespace esphome
