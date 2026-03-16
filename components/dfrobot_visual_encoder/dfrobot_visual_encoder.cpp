40264f86403f6bf521f94b8613e9ba92097b6537
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 15:25:31 +0000

    Update dfrobot_visual_encoder.cpp

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.cpp
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.cpp
@@ -1,22 +1,46 @@
-#pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/components/i2c/i2c.h"
+#include "dfrobot_visual_encoder.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace dfrobot_visual_encoder {
 
-class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
- public:
-  void setup() override;
-  void loop() override;
+static const char *TAG = "dfrobot_visual_encoder";
 
-  void set_led_level(float level);
+void DFRobotVisualEncoder::setup() {
+  ESP_LOGI(TAG, "DFRobot Visual Encoder initialized");
+}
 
- protected:
-  int16_t last_value_{0};
-  uint32_t last_button_{0};
-};
+void DFRobotVisualEncoder::loop() {
+
+  uint8_t data[2];
+
+  if (!this->read(data, 2)) {
+    return;
+  }
+
+  int16_t value = (data[0] << 8) | data[1];
+
+  if (value > last_value_) {
+    ESP_LOGD(TAG, "Clockwise");
+  }
+
+  if (value < last_value_) {
+    ESP_LOGD(TAG, "Counter Clockwise");
+  }
+
+  last_value_ = value;
+}
+
+void DFRobotVisualEncoder::set_led_level(float level) {
+
+  uint8_t brightness = level * 255;
+
+  uint8_t buf[2];
+  buf[0] = 0x10;
+  buf[1] = brightness;
+
+  this->write(buf, 2);
+}
 
 }  // namespace dfrobot_visual_encoder
 }  // namespace esphome
